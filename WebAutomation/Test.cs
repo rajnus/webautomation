18599f8680cec8c3f91017e715957ea7f5a0bb1d
rajnus <rajnus@MacBook-Air.local>
2017-02-26 21:12:40 +0100

    adding to GitHub

---
+++ WebAutomation/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace WebAutomation
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			IWebDriver driver = new ChromeDriver("/Users/rajnus/workspace/WebDriver");
+			driver.Url = "http://google.com";
+			IWebElement element = driver.FindElement(By.Id("lst-ib"));
+			element.SendKeys("test");
+			element.SendKeys(Keys.Enter);
+			Thread.Sleep(6000);
+			driver.Close();
+		}
+	}
+}
