f46ed592999f168b558cbb5b67d40f38d99ddf0c
rajnus <rajnus@hotmail.com>
2017-02-26 21:45:45 +0100

    update

--- WebAutomation/Test.cs
+++ WebAutomation/Test.cs
@@ -17,7 +17,7 @@ namespace WebAutomation
 			IWebElement element = driver.FindElement(By.Id("lst-ib"));
 			element.SendKeys("test");
 			element.SendKeys(Keys.Enter);
-			Thread.Sleep(2000);
+			Thread.Sleep(1000);
 			driver.Close();
 		}
 	}
