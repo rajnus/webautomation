bd91fb371763f8db22b1be84cb58502d316115b6
rajnus <rajnus@hotmail.com>
2017-03-09 17:36:31 +0100

    bwin

--- WebAutomation/Test.cs
+++ WebAutomation/Test.cs
@@ -2,23 +2,44 @@
 using System;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System.Threading;
 
 namespace WebAutomation
 {
 	[TestFixture()]
-	public class Test
+	public class Bwin
 	{
+		public static IWebDriver driver;
+
+		[SetUp]
+		public void SetUp()
+		{
+			
+
+
+			driver = new ChromeDriver("/Users/rajnus/workspace/WebDriver");
+			driver.Url = "https://www.bwin.com/de";
+			//driver.Manage().Timeouts().ImplicityWait(TimeSpan.FromSeconds(5));
+		}
+
 		[Test()]
 		public void TestCase()
 		{
-			IWebDriver driver = new ChromeDriver("/Users/rajnus/workspace/WebDriver");
-			driver.Url = "http://google.com";
-			IWebElement element = driver.FindElement(By.Id("lst-ib"));
-			element.SendKeys("test");
-			element.SendKeys(Keys.Enter);
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+			driver.Url = "https://www.bwin.com/de";
+			wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='header']/div[1]/div[2]/ul/li[2]/a")));
+			driver.FindElement(By.XPath("//*[@id='header']/div[1]/div[2]/ul/li[2]/a")).Click();
+			wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='Input_NameData_FirstName']")));
+			driver.FindElement(By.XPath("//*[@id='Input_NameData_FirstName']")).SendKeys("Igor");
+			driver.FindElement(By.CssSelector("#Input_NameData_LastName")).SendKeys("Rajnovic");
+			driver.FindElement(By.CssSelector("#Input_BirthData_DateOfBirth_Day")).SendKeys("07");
+			driver.FindElement(By.CssSelector("#Input_BirthData_DateOfBirth_Month")).SendKeys("Jul");
+			driver.FindElement(By.CssSelector("#Input_BirthData_DateOfBirth_Year")).SendKeys("1979");
 			Thread.Sleep(3500);
 			driver.Close();
+
+
 		}
 	}
 }
