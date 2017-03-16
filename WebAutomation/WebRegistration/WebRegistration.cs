64f0d406a8a3226db04719f6597d7b3f5f9bf18c
rajnus <rajnus@hotmail.com>
2017-03-17 00:43:15 +0100

    added text boxes

--- WebAutomation/WebRegistration/WebRegistration.cs
+++ WebAutomation/WebRegistration/WebRegistration.cs
@@ -1,32 +1,79 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
 
 namespace WebAutomation
 {
 	public class WebRegistration
 	{
-		public string firstName = "//*[@id='Input_NameData_FirstName']";
-		public string lastName = "#Input_NameData_LastName";
-		public string dateOfBirthDay = "#Input_BirthData_DateOfBirth_Day";
-		public string dateOfBirthMonth = "#Input_BirthData_DateOfBirth_Month";
-		public string dateOfBirthYear = "#Input_BirthData_DateOfBirth_Year";
 
 
-		public void EnterFirstName(string firstName)
-		{
-			Root.driver.FindElement(By.XPath(this.firstName)).SendKeys(firstName);
-		}
+		[FindsBy(How = How.CssSelector, Using = "#Gender>div.radioOptions.check-radio-s2>label:nth-child(2)")]
+		public IWebElement Male { get; set; }
 
-		public void EnterLastName(string lastName)
-		{
-			Root.driver.FindElement(By.CssSelector(this.lastName)).SendKeys(lastName);
-		}
+		[FindsBy(How = How.CssSelector, Using = "#Gender>div.radioOptions.check-radio-s2>label:nth-child(1)")]
+		public IWebElement Female { get; set; }
 
+		[FindsBy(How = How.CssSelector, Using = "#Input_NameData_FirstName")]
+		public IWebElement FirstName{ get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_NameData_LastName")]
+		public IWebElement LastName { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_BirthData_DateOfBirth_Day")]
+		public IWebElement DateOfBirthDay { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_BirthData_DateOfBirth_Month")]
+		public IWebElement DateOfBirthMonth { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_BirthData_DateOfBirth_Year")]
+		public IWebElement DateOfBirthYear { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_AddressData_AddressCountryCode")]
+		public IWebElement Country { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_AddressData_AddressState")]
+		public IWebElement County { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_AddressData_AddressCity")]
+		public IWebElement City { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_AddressData_AddressZip")]
+		public IWebElement PostCode { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_AddressData_AddressLine1")]
+		public IWebElement Address { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_ContactData_EmailAddress")]
+		public IWebElement Email { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#Input_ContactData_MobileCountryCode")]
+		public IWebElement MobileCode { get; set; }
+
+		[FindsBy(How = How.CssSelector, Using = "#continueToStep>button\n")]
+		public IWebElement NextStep { get; set; }
+
+
+
+
+
+
+
+
+
+		/// <summary>
+		/// Enters the date of birth.
+		/// </summary>
+		/// <param name="dateOfBirthDay">Date of birth day.</param>
+		/// <param name="dateOfBirthMonth">Date of birth month.</param>
+		/// <param name="dateOfBirthYear">Date of birth year.</param>
 		public void EnterDateOfBirth(string dateOfBirthDay, string dateOfBirthMonth, string dateOfBirthYear)
 		{
-			Root.driver.FindElement(By.CssSelector(this.dateOfBirthDay)).SendKeys(dateOfBirthDay);
-			Root.driver.FindElement(By.CssSelector(this.dateOfBirthMonth)).SendKeys(dateOfBirthMonth);
-			Root.driver.FindElement(By.CssSelector(this.dateOfBirthYear)).SendKeys(dateOfBirthYear);
+			DateOfBirthDay.SendKeys(dateOfBirthDay);
+			DateOfBirthMonth.SendKeys(dateOfBirthMonth);
+			DateOfBirthYear.SendKeys(dateOfBirthYear);
+		}
 
-		}	
 	}
 }
